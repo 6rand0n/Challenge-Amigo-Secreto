c54c336509fe7a2a86c2c889da3c49e720491244
6rand0n <brandonjavier03@gmail.com>
2025-03-05 22:12:53 -0600

    Terminado

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function AgregarAmigos() {
+    let nombre = document.getElementById("amigo").value;
+
+    if (nombre == "") {
+        alert("Favor de ingresar un nombre!");
+        return;
+    }else{
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        ActualizarLista();
+    }
+}
+
+function ActualizarLista() {
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function SortearAmigo() {
+    if(amigos.length == 0){
+        alert("No hay amigos en la lista!");
+        return;
+    }else{
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.querySelector("#resultado").innerHTML = "El amigo Secreto es: " + amigoSorteado + "!!!";
+    }
+}
